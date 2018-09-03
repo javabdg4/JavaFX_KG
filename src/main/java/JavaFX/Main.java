3b1a71fdb42ba7ec74d714b31b19eec0be2045ef
Kamil Grabowski <kamil.grabowski89x@gmail.com>
2018-09-03 19:07:16 +0200

    add View Methods

--- src/main/java/JavaFX/Main.java
+++ src/main/java/JavaFX/Main.java
@@ -1,15 +1,39 @@
 package JavaFX;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
+    private VBox vBox;
+    private Stage stage;
 
+    public static void main(String[] args) {
+        launch();
     }
 
     public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
+        loadView();
+    }
+
+    public void loadView() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/RootView.fxml"));
+        try {
+            vBox = (VBox) loader.load();
+            Scene scene = new Scene(vBox);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
