16e5eb55a86bcee6f6b845f44b260cad6df5509a
Kamil Grabowski <kamil.grabowski89x@gmail.com>
2018-09-03 18:54:55 +0200

    add Main class

---
+++ src/main/java/JavaFX/Main.java
@@ -0,0 +1,15 @@
+package JavaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
